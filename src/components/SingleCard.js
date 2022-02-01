f9042ac6157d6d0cc68670cb9497845e4db1eb93
Andrew Giang <giang184@gmail.com>
2022-01-31 23:57:20 -0800

    add handle choice

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,21 @@
 import './SingleCard.css'
 import React from 'react';
 
-export default function SingleCard({card}) {
+export default function SingleCard({card, handleChoice}) {
+  const handleClick = () => {
+    handleChoice(card)
+  }
+
   return (          
     <div className="card">
       <div>
         <img className="front" src={card.src} alt="front" />
-        <img className="back" src="/img/cover.png" alt="back" />
+        <img 
+          className="back" 
+          src="/img/cover.png" 
+          onClick={handleClick} 
+          alt="back" 
+        />
       </div>
     </div>
   )
