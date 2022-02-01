acb7a6e9a6db641a1d32c169b402ba4ea779324c
Andrew Giang <giang184@gmail.com>
2022-02-01 00:32:29 -0800

    disabling card selection

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   const [turns, setTurns] = useState(0)
   const [choiceOne, setChoiceOne] = useState(null)
   const [choiceTwo, setChoiceTwo] = useState(null)
-
+  const [disabled, setDisabled] = useState(false)
   //shuffle cards
   const shuffleCards = () => {
     const shuffledCards = [...cardImages, ...cardImages]
@@ -35,6 +35,7 @@ function App() {
   //compare 2 selected cards
   useEffect(() => {
     if(choiceOne && choiceTwo) {
+      setDisabled(true)
       if(choiceOne.src === choiceTwo.src) {
         setCards(prevCards => {
           return prevCards.map(card => {
@@ -58,6 +59,7 @@ function App() {
     setChoiceOne(null)
     setChoiceTwo(null)
     setTurns(prevTurns => prevTurns +1)
+    setDisabled(false)
   }
 
   return (
@@ -72,6 +74,7 @@ function App() {
             card={card}
             handleChoice = {handleChoice}  
             flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
           />
         ))}
       </div>
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,9 +1,11 @@
 import './SingleCard.css'
 import React from 'react';
 
-export default function SingleCard({card, handleChoice, flipped}) {
+export default function SingleCard({card, handleChoice, flipped, disabled}) {
   const handleClick = () => {
-    handleChoice(card)
+    if(!disabled) {
+      handleChoice(card)
+    }
   }
 
   return (          
